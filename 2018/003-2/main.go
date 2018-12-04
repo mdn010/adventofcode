5982dfa7d067142651a53b3dfedcfed5451bab98
Matthew Nguyen <mdn@localhost.localdomain>
2018-12-03 20:19:33 -0800

    utilize fmt.Sscanf

--- 2018/003-2/main.go
+++ 2018/003-2/main.go
@@ -12,7 +12,6 @@ func main() {
 	m := make(map[string]int)
 
 	content, err := ioutil.ReadFile("input.txt")
-	//content, err := ioutil.ReadFile("test2.txt")
 	checkError(err)
 
 	lines := strings.Split(string(content), "\n")
@@ -21,92 +20,39 @@ func main() {
 		if len(v) == 0 {
 			break
 		}
-		//fmt.Printf("%v", v)
-		care := v[strings.IndexByte(v, '@')+2:]
-		//fmt.Println(care)
-		key := care[:strings.IndexByte(care, ':')]
-		//fmt.Println(key)
-		xoff := key[:strings.IndexByte(key, ',')]
-		//fmt.Println(xoff)
-		yoff := key[strings.IndexByte(key, ',')+1:]
-		//fmt.Println(yoff)
-		area := care[strings.IndexByte(care, ':')+2:]
-		//fmt.Println(area)
-		w := area[:strings.IndexByte(area, 'x')]
-		//fmt.Println(w)
-		h := area[strings.IndexByte(area, 'x')+1:]
-		//fmt.Println(h)
 
-		xoffset, err := strconv.Atoi(xoff)
-		checkError(err)
-		wtoi, err := strconv.Atoi(w)
+		var id, xoff, yoff, w, h int
+		_, err := fmt.Sscanf(v, "#%d @ %d,%d: %dx%d", &id, &xoff, &yoff, &w, &h)
 		checkError(err)
 
-		for x := 1; x <= wtoi; x++ {
-			xkey := strconv.Itoa(xoffset + x)
+		for x := 1; x <= w; x++ {
+			xkey := strconv.Itoa(xoff + x)
 
-			yoffset, err := strconv.Atoi(yoff)
-			checkError(err)
-			htoi, err := strconv.Atoi(h)
-			checkError(err)
-			for y := 1; y <= htoi; y++ {
-				ykey := strconv.Itoa(yoffset + y)
+			for y := 1; y <= h; y++ {
+				ykey := strconv.Itoa(yoff + y)
 				lakey := xkey + "," + ykey
-				//fmt.Printf("lakey: %v\n", lakey)
 				m[lakey] += 1
 			}
 		}
 	}
 
-	var counter int
-	for _, v := range m {
-		//fmt.Printf("result: %v %v\n", k, v)
-		if v >= 2 {
-			counter += 1
-			//fmt.Printf("result: %v %v\n", k, v)
-		}
-	}
-
-	//fmt.Println(counter)
-
 	for _, v := range lines {
 		if len(v) == 0 {
 			break
 		}
-		//fmt.Printf("%v", v)
-		care := v[strings.IndexByte(v, '@')+2:]
-		//fmt.Println(care)
-		key := care[:strings.IndexByte(care, ':')]
-		fmt.Println(key)
-		xoff := key[:strings.IndexByte(key, ',')]
-		//fmt.Println(xoff)
-		yoff := key[strings.IndexByte(key, ',')+1:]
-		//fmt.Println(yoff)
-		area := care[strings.IndexByte(care, ':')+2:]
-		//fmt.Println(area)
-		w := area[:strings.IndexByte(area, 'x')]
-		//fmt.Println(w)
-		h := area[strings.IndexByte(area, 'x')+1:]
-		//fmt.Println(h)
-
-		xoffset, err := strconv.Atoi(xoff)
-		checkError(err)
-		wtoi, err := strconv.Atoi(w)
-		checkError(err)
 
 		var nottheone bool
 
-		for x := 1; x <= wtoi; x++ {
-			xkey := strconv.Itoa(xoffset + x)
+		var id, xoff, yoff, w, h int
+		_, err := fmt.Sscanf(v, "#%d @ %d,%d: %dx%d", &id, &xoff, &yoff, &w, &h)
+		checkError(err)
 
-			yoffset, err := strconv.Atoi(yoff)
-			checkError(err)
-			htoi, err := strconv.Atoi(h)
-			checkError(err)
-			for y := 1; y <= htoi; y++ {
-				ykey := strconv.Itoa(yoffset + y)
+		for x := 1; x <= w; x++ {
+			xkey := strconv.Itoa(xoff + x)
+
+			for y := 1; y <= h; y++ {
+				ykey := strconv.Itoa(yoff + y)
 				lakey := xkey + "," + ykey
-				//fmt.Printf("lakey: %v\n", lakey)
 				if m[lakey] != 1 {
 					nottheone = true
 				}
