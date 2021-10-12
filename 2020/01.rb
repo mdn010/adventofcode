9dbff5f927ab8bf4bd80281d52cd020ea2a2cccd
Matt Nguyen <mattn428@gmail.com>
2021-10-12 13:55:09 -0700

    Solved Part 2

--- 2020/01.rb
+++ 2020/01.rb
@@ -1,3 +1,5 @@
 arr = File.read('input.txt').split.map(&:to_i)
 
-answer = arr.combination(2).find { |a| a.reduce(0, :+) == 2020 }.reduce(:*)
+answer0 = arr.combination(2).find { |a| a.reduce(0, :+) == 2020 }.reduce(:*)
+
+answer1 = arr.combination(3).find { |a| a.reduce(0, :+) == 2020 }.reduce(:*)
